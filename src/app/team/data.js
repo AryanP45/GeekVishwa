c9910381291858f0c389d84fa3d955b078f9c995
Yash <yanbhore@gmail.com>
2023-10-23 16:33:25 +0530

    Teams- Core,Technical,Design

--- src/app/team/data.js
+++ src/app/team/data.js
@@ -1,57 +1,86 @@
 
 export default [
     {
-        img: "../../../public/assets/images/1.png",
-        title: "Aryan ",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+        core: [
+
+
+            {
+                img: "/assets/Team/Pratham Patil.png",
+                title: "Pratham Patil ",
+                pos: "President",
+                team:'core',
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/Team/AmanKasat - B2_33 Aman Kasat.jpg",
+                title: "Aman Kasat  ",
+                pos: "Vice President",
+                team:'core',
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            
+            {
+                img: "/assets/Team/RENUKA - L_1276_RENUKA WANKHADE.jpg",
+                title: "Renuka Wankhade",
+                pos: "General Manager",
+                team: "core",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+           
+        ],
+        tech: [
+            {
+                img: "/assets/Team/Screenshot_20230907-155055_Gallery - PATIL ARYAN.jpg",
+                title: "Aryan Patil",
+                pos: "Coding Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/Team/IMG20230714184124 - 711_KANHAIYA.jpg",
+                title: "Kanhaiya Sulay ",
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/Team/Karan.jpg - 4022_KARAN _GULVE.jpg",
+                title: "Karan Gulve",
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/1.png",
+                title: "Heramb Patil ",
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+           
+        ],
+        design: [
+            {
+                img: "/assets/Team/IMG-20230906-WA0107 - BHAGYASHREE GANESH.jpg",
+                title: "Bhagyashree Tekade",
+                pos: "Design Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/Team/IMG_20230909_121357 - 545_VAISHNAVI PATIL.jpg",
+                title: "Vaishnavi Patil",
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/Team/IMG_20230902_084214 - 2061_SANSKRUTI BURKULE.jpg",
+                title: "Sanskruti Burkule" ,
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+            {
+                img: "/assets/1.png",
+                title: "Isha Varade ",
+                pos: "Associate Head",
+                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+            },
+           
+        ],
     },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "Yash",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "Mahi",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "Sagar",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "Abcs",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "ADI",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "ADI",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "ADi",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-    {
-        img: "../../../public/assets/images/1.png",
-        title: "ADI",
-        pos:"President",
-        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-    },
-]
+];
--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -2,9 +2,7 @@ import TeamCards from '../../component/TeamCards.jsx';
 const Page = () => {
     return (
         <div>
-            <div className='mt-[5.2%] text-[30px] text-white flex justify-center font-black'>
-                <span>Our Team</span>
-            </div>
+      
             <div className='mt-[3rem]'>
                 <TeamCards/>
             </div>
